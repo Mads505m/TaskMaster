21dc829817b3b2b1122a893429efa91cd55cc996
Mads505m <onlinebigchungus@gmail.com>
2024-10-10 21:09:54 +0200

    a

--- Controller/Router.js
+++ Controller/Router.js
@@ -15,37 +15,42 @@ routes.post('/tasks', (req, res) => {
     const newTask = req.body.task;
     if (newTask && newTask.trim() !== '') {
         tasks.push(newTask);
-        res.status(201).json(newTask);
+        return res.status(201).json(newTask);
     } else {
         console.error('Received empty task');
+        return res.status(400).json({ message: 'Task cannot be empty' });
     }
-    return res.status(400).json({ message: 'Task cannot be updated' });
 });
 
-routes.put('/tasks',(req, res) => {
+routes.put('/tasks', (req, res) => {
     const { oldTask, newTask } = req.body;
     const taskIndex = tasks.indexOf(oldTask);
-    if (taskIndex !== -1 && oldTask !== newTask) {
-        tasks.splice(taskIndex, 1, newTask);
-        return res.status(204).send();
-    } else if(oldTask === newTask) {
-        console.error('Cannot update with same description');
-    } else{
-        console.error('Unexpected error occurred')
+
+    if (taskIndex !== -1) {
+        if (oldTask !== newTask) {
+            tasks.splice(taskIndex, 1, newTask);
+            return res.status(204).send(); // No content response
+        } else {
+            console.error('Cannot update with same description');
+            return res.status(400).json({ message: 'Cannot update with the same description' });
+        }
+    } else {
+        console.error('Task not found');
+        return res.status(404).json({ message: 'Task not found' });
     }
-    return res.status(404).json({ message: 'Task not found' });
-})
+});
 
 routes.delete('/tasks', (req, res) => {
     const taskToDelete = req.body.task;
     const taskIndex = tasks.indexOf(taskToDelete);
+
     if (taskIndex !== -1) {
         tasks.splice(taskIndex, 1);
         return res.status(204).send();
-    } else{
-        console.error('Unexpected error occurred')
+    } else {
+        console.error('Task not found');
+        return res.status(404).json({ message: 'Task not found' });
     }
-    return res.status(404).json({ message: 'Task not found' });
 });
 
 module.exports = routes;
